f58d8d52e6c5687a4104ddb145ba7cba690a0759
hlolli <hlolli@gmail.com>
2017-01-15 17:04:20 +0100

    indentation rules work

--- csound-mode.el
+++ csound-mode.el
@@ -18,47 +18,97 @@
     (define-key map "\C-j" 'newline-and-indent) map)
   "Keymap for csound-mode")
 
-(defun csound-indent-xml-line? ()
-  (save-excursion
-    (beginning-of-line 0)
-    (search-forward-regexp "\\<.+\\>" (line-end-position 2) t)))
+(defcustom csound-indentation-spaces 2
+  "Set how many spaces are in indentation"
+  :type 'integer
+  :group 'csound-mode)
 
-(defun csound-indent-definstr-line? ()
+;; (defun csound-indent-xml-line? ()
+;;   (save-excursion
+;;     (beginning-of-line 0)
+;;     (search-forward-regexp "\\<.+\\>" (line-end-position 2) t)))
+
+(defun csound-indent-begin-of-expr? ()
   (save-excursion
-    (beginning-of-line 0)
-    (search-forward-regexp "\\(instr\s\\)+" (line-end-position 2) t)))
+    (beginning-of-line 1)
+    (search-forward-regexp "\\b\\(instr\\)\\b\\|\\b\\(opcode\\)\\b" (line-end-position 1) t)))
+
+(defun csound-indent-end-of-expr? ()
+  (save-excursion
+    (beginning-of-line 1)
+    (search-forward-regexp "\\b\\(endin\\)\\b\\|\\b\\(endop\\)\\b" (line-end-position 1) t)))
 
 (defun csound-indent-inside-instr? ()
-  (interactive)
-  (let* ((last-instr (save-excursion (search-backward "\\(instr\s\\)+" (beginning-of-buffer) t 1)))
-	 (lest-endin (save-excursion (search-backward "endin" (beginning-of-buffer) t 1))))
-    (if (not (numberp last-instr))
-	;; nil
-	(message "nei1")
-      (if (not (numberp last-endin))
-	  ;; t
-	  (message "já1")
-	(let ((delta (- last-instr last-endin)))
-	  (if (< 0 delta)
-	      (message "já2") (message "nei2")
-	      ;; t nil
-	      ))))))
+  ;; (interactive)
+  (let* ((last-instr (save-excursion (search-backward-regexp "\\(instr\\)\\b" nil t)))
+	 (last-endin (save-excursion (search-backward-regexp "\\(endin\\)\\b" nil t))))
+    (cond ((eq 'nil last-instr) nil)
+	  ((and (numberp last-instr) (eq 'nil last-endin)) t)
+	  ((< last-endin last-instr) t)
+	  (t nil))))
 
-(defun henda ()
-  (interactive)
-  (beginning-of-line 0)
-  ;;(search-backward "CsScore" (line-beginning-position))
-  )
+(defun csound-indent-inside-opcode? ()
+  ;; (interactive)
+  (let* ((last-opcode (save-excursion (search-backward-regexp "\\(opcode\\)\\b" nil t)))
+	 (last-endop (save-excursion (search-backward-regexp "\\(endop\\)\\b" nil t))))
+    (cond ((eq 'nil last-opcode) nil)
+	  ((and (numberp last-opcode) (eq 'nil last-endop)) t)
+	  ((< last-endop last-opcode) t)
+	  (t nil))))
 
 
-(defun csound-indent-line () (csound-indent-definstr-line?))
+(defun recursive-count (regex string start)
+  (if (string-match regex string start)
+      (+ 1 (recursive-count regex string (match-end 0)))
+    0))
 
+(defun csound-indent-end-of-bool? ()
+  (save-excursion
+    (beginning-of-line 1)
+    (if (search-forward-regexp
+	 "\\b\\(endif\\)\\b\\|\\b\\(od\\)\\b\\|\\b\\(else\\)\\b\\|\\b\\(elseif\\)\\b"
+	 (line-end-position 2) t)
+	t nil)))
+
+(defun csound-indent-inside-expression-calc (expr-type)
+  (let* ((beginning-of-expr (if (eq 'instr expr-type)
+				(save-excursion
+				  (search-backward-regexp "\\(instr\\)\\b" nil t))
+			      (save-excursion
+				(search-backward-regexp "\\(opcode\\)\\b" nil t))))
+	 (count-if-statements (recursive-count "\\b\\(if\\)\\b" (buffer-substring beginning-of-expr (line-end-position 1)) 0))
+	 (count-elseif-statements (recursive-count "\\b\\(elseif\\)\\b" (buffer-substring beginning-of-expr (line-end-position 1)) 0))
+	 (count-endif-statements (recursive-count "\\b\\(endif\\)\\b" (buffer-substring beginning-of-expr (line-end-position 1)) 0))
+	 (count-while-statements (recursive-count "\\b\\(while\\)\\b" (buffer-substring beginning-of-expr (line-end-position 1)) 0))
+	 (count-od-statements (recursive-count "\\b\\(od\\)\\b" (buffer-substring beginning-of-expr (line-end-position 1)) 0))
+	 (end-of-bool? (csound-indent-end-of-bool?))
+	 (tab-count (max 1 (1+ (- (+ count-if-statements
+				     count-elseif-statements
+				     count-while-statements) 
+				  count-endif-statements 
+				  count-od-statements))))) 
+    ;; (message "%d" tab-count)
+    (when (eq 't end-of-bool?)
+      ;; (message "end of bool!")
+      (indent-line-to (* csound-indentation-spaces (1- tab-count))))
+    (save-excursion 
+      (indent-line-to (* csound-indentation-spaces tab-count))
+      ;; (beginning-of-line 2)
+      (indent-to (* csound-indentation-spaces tab-count)))))
 
 (defun csound-indent-line ()
   "Indent current line."
-  (interactive)
-  (cond ((csound-indent-xml-line?) (indent-to 0)
-	 (csound-indent-definstr-line?) (indent-to 2))))
+  ;;(interactive)
+  (cond ;;((csound-indent-xml-line?) (indent-to 0))
+   ((csound-indent-begin-of-expr?) (save-excursion
+				     (indent-line-to 0)
+				     (beginning-of-line 2)
+				     (indent-line-to csound-indentation-spaces)))
+   ((csound-indent-end-of-expr?) (indent-line-to 0))
+   ((csound-indent-inside-instr?) (csound-indent-inside-expression-calc 'instr))
+   ((csound-indent-inside-opcode?) (csound-indent-inside-expression-calc 'opcode)) 
+   
+   (t (indent-to 0))))
 
 
 (defun opcode-completion-at-point ()
